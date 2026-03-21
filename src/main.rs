785fa4eb504ac8c588dd8449d0d122ecf10b4533
Narender9995 <narender.pagidimarri@omniworksindia.com>
2026-03-21 19:01:33 +0530

    Read Headers

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use std::net::TcpListener;
 
 const SUCCESS_RESPONSE: &[u8] = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
 const ERROR_RESPONSE: &[u8] = "HTTP/1.1 400 NOT FOUND\r\n\r\n".as_bytes();
+
+type Key = String;
+type Value = String;
 fn main() -> Result<()> {
     env_logger::init();
     info!("Server started");
@@ -17,8 +20,21 @@ fn main() -> Result<()> {
 
                 let mut request_buffer = BufReader::new(&stream);
                 let mut request_line = String::new();
-
                 request_buffer.read_line(&mut request_line)?;
+
+                let mut headers: Vec<(Key, Value)> = Vec::new();
+                loop {
+                    let mut header_line = String::new();
+                    let next_header = request_buffer.read_line(&mut header_line)?;
+                    if header_line == "\r\n" || next_header == 0 {
+                        break;
+                    }
+                    let Some((key, value)) = header_line.split_once(":") else {
+                        error!("Invalid header line in request: {header_line}");
+                        continue;
+                    };
+                    headers.push((key.to_string(), value.to_string()));
+                }
 
                 //split the first line by space
                 //secont totken is the path
@@ -32,6 +48,32 @@ fn main() -> Result<()> {
                             debug!("root path requested");
                             stream.write(SUCCESS_RESPONSE)?;
                             stream.flush()?;
+                        } else if path == "/user-agent/" {
+                            let mut user_agent = None;
+                            for (key, value) in headers {
+                                if key == "User-Agent" {
+                                    user_agent = Some(value);
+                                    break;
+                                }
+                            }
+                            match user_agent {
+                                None => {
+                                    error!(
+                                        "User-Agent header not found in request . Request: {request_line}"
+                                    );
+                                    stream.write(ERROR_RESPONSE)?;
+                                    stream.flush()?;
+                                }
+                                Some(user_agent) => {
+                                    let response = format!(
+                                        "HTTP1.1 200 OK\r\nContent-Type: text/plain\r\nContent=length: {}\r\n\r\n{}",
+                                        user_agent.len(),
+                                        user_agent
+                                    );
+                                    stream.write(response.as_bytes())?;
+                                    stream.flush()?;
+                                }
+                            }
                         } else if path.starts_with("/echo/") {
                             let echo_path = path.split_once("/echo/");
                             match echo_path {
